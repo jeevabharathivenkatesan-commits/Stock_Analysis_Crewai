7f9666a410d3ee7d64906ec5fb9160678b8b1de4
jeevabharathivenkatesan-commits <jeevabharathivenkatesan@gmail.com>
2026-06-21 19:04:02 +0530

    Refactor import statements in crew.py

--- crew.py
+++ crew.py
@@ -1,10 +1,10 @@
 from crewai import Crew
 
-from agent.analyst_agent import analyst_agent
-from agent.trader_agent import trader_agent
-from task.analyst_task import analyst_task
-from task.trader_task import trader_task
-#from tool.stock_research_tool import get_stock_price
+from analyst_agent import analyst_agent
+from trader_agent import trader_agent
+from analyst_task import analyst_task
+from trader_task import trader_task
+#from stock_research_tool import get_stock_price
 
 stock_crew=Crew(
     agents=[analyst_agent,trader_agent],
